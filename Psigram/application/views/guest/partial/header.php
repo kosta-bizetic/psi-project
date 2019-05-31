e3cbbfdd21bb3b2a264337cf43a42ac90dace1b4
Luka Dojcilovic <l.dojcilovic@gmail.com>
2019-05-31 22:29:37 +0200

    Reorganized navbars.

--- Psigram/application/views/guest/partial/header.php
+++ Psigram/application/views/guest/partial/header.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <a class="navbar-brand" href="<?php echo site_url("Guest/login")?>">Psigram</a>
     <div class="collapse navbar-collapse" id="navbarColor01">
-    <ul class="navbar-nav mr-auto">
+    <ul class="navbar-nav ml-auto">
         <a class="nav-link" href="<?php echo site_url("Guest/login")?>">Log In</a>
       </li>
       <li class="nav-item">
--- Psigram/application/views/user/partial/header.php
+++ Psigram/application/views/user/partial/header.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <a class="navbar-brand" href="<?php echo site_url("User/feed")?>">Psigram</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
+  <a style="padding-right: 8.75%" class="navbar-brand" href="<?php echo site_url("User/feed")?>">Psigram</a>
   <div class="collapse navbar-collapse" id="navbarColor01">
-    <ul class="navbar-nav mr-auto">
+    <form class="form-inline my-2 my-lg-0 ml-auto mr-auto" method="post" action="<?php echo site_url("User/search");?>">
+      <input class="form-control mr-sm-2" type="text" name="search_text" placeholder="Search">
+      <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+    </form>
+    <ul class="navbar-nav">
       <li class="nav-item">
-        <a class="nav-link" href="<?php echo site_url("User/feed")?>">Feed<span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="<?php echo site_url("User/feed")?>">Feed</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="<?php echo site_url("User/addPost")?>">Add post</a>
@@ -21,10 +21,6 @@
         <a class="nav-link" href="<?php echo site_url("User/logOut")?>">Log Out</a>
       </li>
     </ul>
-      <form class="form-inline my-2 my-lg-0" method="post" action="<?php echo site_url("User/search");?>">
-          <input class="form-control mr-sm-2" type="text" name="search_text" placeholder="Search">
-      <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-    </form>
   </div>
 </nav>
 
