40ca823e0cd9c11719145a9ac5b8cc66a9fcaee2
Luka Dojcilovic <l.dojcilovic@gmail.com>
2019-05-31 13:54:00 +0200

    Refactored User controller.

--- Psigram/application/controllers/User.php
+++ Psigram/application/controllers/User.php
@@ -14,6 +14,7 @@
 class User extends CI_Controller {
 
     var $data = array();
+    var $user;
 
     public function __construct() {
         parent::__construct();
@@ -26,6 +27,8 @@ class User extends CI_Controller {
         $this->load->model('UserModel');
 
         $this->data['title'] = 'Psigram';
+
+        $this->user = $this->session->userdata['user'];
     }
 
     public function index() {
@@ -43,9 +46,8 @@ class User extends CI_Controller {
     }
 
     public function addPostHandler() {
-        $user = $this->session->userdata['user'];
         $config['upload_path']          = 'uploads//';
-        $config['file_name']            = $user->username."_".time();
+        $config['file_name']            = $this->user->username."_".time();
         $config['allowed_types']        = 'gif|jpg|png';
         $config['max_size']             = 2000;
         $config['max_width']            = 1024*4;
@@ -57,18 +59,17 @@ class User extends CI_Controller {
                 $this->data['error'] = $this->upload->display_errors();
                 $this->addPost();
         } else {
-            $this->PostModel->addPost($config['file_name'].$this->upload->data('file_ext'), $user->id_user );
+            $this->PostModel->addPost($config['file_name'].$this->upload->data('file_ext'), $this->user->id_user );
 
             redirect("User/feed");
         }
     }
 
     public function profile() {
-        $user = $this->session->userdata['user'];
-        $this->data['user'] = $user;
-        $this->data['num_posts'] = $this->UserModel->getNumberOfPosts($user->id_user);
-        $this->data['num_followers'] = $this->UserModel->getNumberOfFollowers($user->id_user);
-        $this->data['num_following'] = $this->UserModel->getNumberOfFollowing($user->id_user);
+        $this->data['user'] = $this->user;
+        $this->data['num_posts'] = $this->UserModel->getNumberOfPosts($this->user->id_user);
+        $this->data['num_followers'] = $this->UserModel->getNumberOfFollowers($this->user->id_user);
+        $this->data['num_following'] = $this->UserModel->getNumberOfFollowing($this->user->id_user);
         $this->load->view('user/profile.php', $this->data);
     }
 
