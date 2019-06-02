456c0d1092993a4a012d1c4376e0e899cd293797
kosta-bizetic <kosta.bizetic@gmail.com>
2019-06-02 09:49:03 +0200

    Added partial view head.php

--- Psigram/application/views/user/search.php
+++ Psigram/application/views/user/search.php
@@ -6,9 +6,8 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <?php $this->load->view('partial/head.php', $this->data); ?>
+        
         <style>
             td {
                 text-align:center;
@@ -20,7 +19,6 @@ and open the template in the editor.
               width: 100% ;
             }
         </style>
-        <title><?php echo $title.' - '.$posttitle ?></title>
     </head>
     <body>
         <?php
