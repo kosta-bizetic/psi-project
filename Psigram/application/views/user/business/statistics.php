ef6762103b9add9f5eb7ae1a5d153a800e6f6b19
Luka Dojcilovic <l.dojcilovic@gmail.com>
2019-06-04 14:06:11 +0200

    Fix

---
+++ Psigram/application/views/user/business/statistics.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <?php $this->load->view('partial/head.php', $this->data); ?>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.min.js"></script>
+        <style>
+            th, td {
+                text-align:center;
+                vertical-align:middle;
+            }
+        </style>
+    </head>
+    <body>
+        <?php
+            $this->load->view('user/partial/header.php', $this->data);
+
+            $this->load->view('user/partial/userProfile.php', $this->data);
+        ?>
+
+        <div class="container-fluid">
+            <div class="row justify-content-center">
+                <canvas id="genderPie" class="col-md-4"></canvas>
+                <canvas id="ageHisto" class="col-md-4"></canvas>
+            </div>
+        </div>
+        <script>
+            var genderCtx = document.getElementById('genderPie').getContext('2d');
+            new Chart(genderCtx, {
+                    type: 'pie',
+                    data: {
+                            datasets: [{
+                                    data: [
+                                        <?php echo $gender[0] ?>,
+                                        <?php echo $gender[1] ?>
+                                    ],
+                                    backgroundColor: [
+                                            'rgba(54, 162, 235, 0.5)',
+                                            'rgba(255, 99, 132, 0.5)',
+                                    ],
+                                    label: 'Gender'
+                            }],
+                            labels: [
+                                    'Male',
+                                    'Female'
+                            ]
+                    },
+                    options: {
+                            responsive: true
+                    }
+            });
+
+            var ageCtx = document.getElementById('ageHisto').getContext('2d');
+            new Chart(ageCtx, {
+                    type: 'bar',
+                    data: {
+                            datasets: [{
+                                    data: [
+                                        <?php echo $age[0] ?>,
+                                        <?php echo $age[1] ?>,
+                                        <?php echo $age[2] ?>,
+                                        <?php echo $age[3] ?>,
+                                        <?php echo $age[4] ?>,
+                                    ],
+                                    backgroundColor: 'rgba(54, 162, 235, 0.5)',
+                                    label: 'Number of followers',
+                                    xAxisID: 0
+                            }],
+                            labels: [
+                                    '18-25',
+                                    '26-40',
+                                    '41-60',
+                                    '61-80',
+                                    '81+'
+                            ]
+
+                    },
+                    options: {
+                        responsive: true,
+                        scales: {
+                            yAxes: [{
+                                ticks: {
+                                    beginAtZero: true
+                                }
+                            }]
+                        }
+                    }
+            });
+
+        </script>
+    </body>
+</html>
---
+++ Psigram/application/views/user/partial/userProfile.php
@@ -0,0 +1,37 @@
+
+<div class="container-fluid">
+    <div class="row justify-content-center">
+        <table class="table col-md-8">
+            <tbody>
+              <tr class="table-primary">
+                    <th scope="row">@<?php echo $user->username; ?></th>
+                    <td><?php echo $user->name.' '.$user->surname; ?></td>
+
+                    <?php
+                        if ($user->id_user != $this->session->userdata['user']->id_user) {
+                            if (!$follows) {
+                                echo '<td><a href="'.site_url("$this->class_name/followHandler/".$user->id_user).'"><button type="button" class="btn btn-success">Follow</button></a></td>';
+                            } else {
+                                echo '<td><a href="'.site_url("$this->class_name/unfollowHandler/".$user->id_user).'"><button type="button" class="btn btn-danger">Unfollow</button></a></td>';
+                            }
+                        } else {
+                            echo '<td><a href="'.site_url("$this->class_name/editProfile").'"><button type="button" class="btn btn-primary">Edit Profile</button></a></td>';
+                        }
+
+                        if ($this->session->userdata['user']->type == 'b') {
+                            echo '<td><a href="'.site_url("$this->class_name/statistics").'"><button type="button" class="btn btn-primary">Statistics</button></a></td>';
+                        }
+
+                        for ($i = 0; $i < 20; $i++) {
+                            echo '<td></td>';
+                        }
+                    ?>
+
+                    <td><?php echo $user->num_posts; ?><br>Posts</td>
+                    <td><a href="<?php echo site_url("$this->class_name/followers/".$user->id_user)?>"><?php echo $user->num_followers; ?><br>Followers</a></td>
+                    <td><a href="<?php echo site_url("$this->class_name/following/".$user->id_user)?>"><?php echo $user->num_following; ?><br>Following</a></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
