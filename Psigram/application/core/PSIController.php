2ea0f94c41b42f15587defa91bbaaf711a384c2d
kosta-bizetic <kosta.bizetic@gmail.com>
2019-06-06 15:58:02 +0200

    Added comments to PSIController.php and MUser.php

--- Psigram/application/core/PSIController.php
+++ Psigram/application/core/PSIController.php
@@ -1,5 +1,6 @@
 <?php
 /**
+ * @author Luka Dojcilovic 2016/0135
  * @author Kosta Bizetic 2016/0121
  */
 
@@ -18,8 +19,16 @@ class PSIController extends CI_Controller {
         $this->form_validation->set_error_delimiters('<font color="red">', '</font>');
     }
 
+    /**
+     * Method which remaps controller function calls.
+     *
+     * @param string $method - Original name of the called method.
+     * @param type $params - Original parameters of the called method.
+     * @return void
+     */
     public function _remap($method, $params = array())
     {
+        return $this->data['posttitle'] = implode(' ', preg_split('/(?=[A-Z])/', ucfirst($method)));
         $this->preparePosttitle($method);
         $this->data['title'] = 'Psigram';
         $this->class_name = get_class($this);
@@ -37,14 +46,21 @@ class PSIController extends CI_Controller {
         show_404();
     }
 
+    /**
+     * Redirects the user to the uri before the last request.
+     *
+     * @return void
+     */
     protected function redirectToLastURI() {
         redirect($this->session->userdata['old_uri']);
     }
 
-    protected function preparePosttitle($function) {
-        return $this->data['posttitle'] = implode(' ', preg_split('/(?=[A-Z])/', ucfirst($function)));
-    }
-
+    /**
+     * Redirects user to the correct controller based on his user type.
+     *
+     * @param type $method - Name of the method to redirect the user to.
+     * @return void
+     */
     protected function redirectToType($method='feed') {
         switch ($this->session->userdata('user')->type) {
             case 'a': redirect("Admin/$method");
--- Psigram/application/models/MUser.php
+++ Psigram/application/models/MUser.php
@@ -1,11 +1,31 @@
 <?php
+/**
+ * @author Luka Dojcilovic 2016/0135
+ * @author Kosta Bizetic 2016/0121
+ */
 
-
+/**
+ * MUser - Model for the user database table.
+ *
+ * @version 1.0
+ */
 class MUser extends CI_Model {
+
+    /**
+    * Creating a new instance.
+    *
+    * @return void
+     */
     public function __construct() {
         parent::__construct();
     }
 
+    /**
+     * Method that gets user data from the database.
+     *
+     * @param string $username - username of the user.
+     * @return Object containing user data
+     */
     public function getUserByUsername($username) {
         return $this->db->from('User')
                         ->where('username', $username)
@@ -13,6 +33,12 @@ class MUser extends CI_Model {
                         ->row();
     }
 
+    /**
+     * Method that gets user data from the database.
+     *
+     * @param int $id_user - id of the user.
+     * @return Object containing user data.
+     */
     public function getUser($id_user) {
         return $this->db->from('User')
                         ->where('id_user', $id_user)
@@ -20,12 +46,12 @@ class MUser extends CI_Model {
                         ->row();
     }
 
-    public function getAllUsers() {
-        return $this->db->from('User')
-                        ->get()
-                        ->result();
-    }
-
+    /**
+     * Method which searches the database for users based on search text.
+     *
+     * @param string $search_text - Text to search for.
+     * @return Array of objects containing user data.
+     */
     public function searchUsers($search_text) {
         $words = preg_split("/[\s]+/", strtolower($search_text));
         $this->db   ->from('User')
@@ -40,40 +66,36 @@ class MUser extends CI_Model {
                         ->result();
     }
 
-    private function prepareToGetFollowers($id_user) {
+    /**
+     * Helper method which prepares the query to get followers of a given user
+     * from the database.
+     *
+     * @param int $id_user - id of the given user.
+     */
+    protected function prepareToGetFollowers($id_user) {
         $this->db   ->from('Follows')
                     ->join('User', 'User.id_user = Follows.id_user_following')
                     ->where('Follows.id_user_followed', $id_user);
     }
 
-
+    /**
+     * Method which gets all followers of a given user from the database.
+     *
+     * @param int $id_user - id of the given user.
+     * @return Array of objects containing user data.
+     */
     public function getFollowers($id_user) {
         $this->prepareToGetFollowers($id_user);
         return $this->db->get()
                         ->result();
     }
 
-    public function getFollowersGenderStatistics($id_user) {
-        $this->prepareToGetFollowers($id_user);
-        $this->db   ->select('gender, COUNT(*) as num_followers')
-                    ->group_by('gender');
-
-        return $this->db->get()->result();
-    }
-
-    public function getFollowersAgeStatistics($id_user) {
-        $age_bounds = array(array(18, 25), array(26, 40), array(41, 60), array(61, 80), array(81, 1000));
-        $age = array();
-        foreach ($age_bounds as $age_bound) {
-            $this->prepareToGetFollowers($id_user);
-            $this->db   ->where("TIMESTAMPDIFF(YEAR, date_of_birth, CURDATE()) >=", $age_bound[0])
-                        ->where("TIMESTAMPDIFF(YEAR, date_of_birth, CURDATE()) <=", $age_bound[1]);
-            array_push($age, $this->db->count_all_results());
-        }
-
-        return $age;
-    }
-
+    /**
+     * Method which gets all users that a given user follows from the database.
+     *
+     * @param int $id_user - id of the given user.
+     * @return Array of objects containing user data.
+     */
     public function getFollowing($id_user) {
         return $this->db->from('Follows')
                         ->join('User', 'User.id_user = Follows.id_user_followed')
@@ -82,12 +104,63 @@ class MUser extends CI_Model {
                         ->result();
     }
 
+    /**
+     * Method which gets followers gender statistics of a given user
+     * from the database.
+     *
+     * @param int $id_user - id of the given user.
+     * statistics should be gotten.
+     * @return Array of objects containing followers gender statistic data.
+     */
+    public function getFollowersGenderStatistics($id_user) {
+        $this->prepareToGetFollowers($id_user);
+        $this->db   ->select('gender, COUNT(*) as num_followers')
+                    ->group_by('gender');
 
+        return $this->db->get()->result();
+    }
 
+    /**
+     * Method which gets followers age statistics of a given user
+     * from the database.
+     *
+     * @param int $id_user - id of the user whose followers age
+     * statistics should be gotten.
+     * @return Array of objects containing followers age statistic data.
+     */
+    public function getFollowersAgeStatistics($id_user) {
+        $age_bounds = array(array(18, 25), array(26, 40), array(41, 60),
+                            array(61, 80), array(81, 1000));
+        $age = array();
+        foreach ($age_bounds as $age_bound) {
+            $this->prepareToGetFollowers($id_user);
+            $condition = "TIMESTAMPDIFF(YEAR, date_of_birth, CURDATE()) >=";
+            $this->db   ->where($condition, $age_bound[0])
+                        ->where($condition, $age_bound[1]);
+            array_push($age, $this->db->count_all_results());
+        }
+
+        return $age;
+    }
+
+    /**
+     * Method which adds a new user to the database.
+     *
+     * @param Array $data - Array which contains all the required data for a
+     * user to be added to the database.
+     * @return void
+     */
     public function addUser($data) {
         $this->db->insert('user', $data);
     }
 
+    /**
+     * Checks whether a username does not exists in the database excluding the
+     * logged in user's username.
+     *
+     * @param string $username - Username to check for.
+     * @return boolean - TRUE if does not exist, FALSE otherwise.
+     */
     public function otherUsernameDoesntExist($username) {
         return $this->db->from('User')
                         ->where('username', $username)
@@ -96,6 +169,13 @@ class MUser extends CI_Model {
                         ->row() == NULL;
     }
 
+    /**
+     * Checks whether an email does not exists in the database excluding the
+     * logged in user's email.
+     *
+     * @param string $email - Email to check for.
+     * @return boolean - TRUE if does not exist, FALSE otherwise.
+     */
     public function otherEmailDoesntExist($email) {
         return $this->db->from('User')
                         ->where('email', $email)
@@ -104,11 +184,24 @@ class MUser extends CI_Model {
                         ->row() == NULL;
     }
 
+    /**
+     * Updates a given user's data in the database.
+     *
+     * @param int $id_user - id of the given user.
+     * @param Array $data - Array which contains the updated user's data.
+     * @return void
+     */
     public function updateUser($id_user, $data) {
         $this->db   ->where('id_user', $id_user)
                     ->update('User', $data);
     }
 
+    /**
+     * Validates a log in
+     *
+     * @param string $password - Password to validate.
+     * @return boolean - TRUE if the credentials are correct, FALSE otherwise.
+     */
     public function logInValidation($password) {
         $username = $this->input->post('username');
         $user = $this->getUserByUsername($username);
